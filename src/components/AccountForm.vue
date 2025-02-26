248a669c37449fe4337da1ec75d4c827d4961d0c
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-26 22:01:51 +0300

    Updated component 'AccountForm' - Added interface 'Account' and const 'accounts'. Added new methods 'addAccount', 'updateAccount' and 'deleteAccount'

--- src/components/AccountForm.vue
+++ src/components/AccountForm.vue
@@ -5,10 +5,79 @@
     <v-btn color="primary" @click="addAccount">
       <v-icon icon="mdi-plus"></v-icon>
     </v-btn>
-    <AccountItem />
+    <v-row>
+      <v-col cols="12" md="6" v-for="account in accounts" :key="id">
+        <AccountItem
+          :account="account"
+          @update="updateAccount(index, $event)"
+          @delete="deleteAccount(index)"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import AccountItem from "./AccountItem.vue";
+
+interface Account {
+  id: number;
+  label: string;
+  recordType: string;
+  login: string;
+  password?: string;
+}
+
+const accounts = ref<Account[]>([
+  {
+    id: 1,
+    label: "XXX",
+    recordType: "Локальная",
+    login: "Значение1",
+    password: "Pyfxtybt",
+  },
+  {
+    id: 2,
+    label: "YYY",
+    recordType: "Локальная",
+    login: "Значение2",
+    password: "Pyfxtybtyna",
+  },
+  {
+    id: 3,
+    label: "XXX; YYY",
+    recordType: "Локальная",
+    login: "Значение3",
+    password: "Pyfxtybtyna823",
+  },
+  {
+    id: 4,
+    label: "MMM",
+    recordType: "LDAP",
+    login: "",
+    password: "",
+  },
+  { id: 5, label: "VVV", recordType: "LDAP", login: "", password: "" },
+]);
+
+const addAccount = () => {
+  accounts.value.push({
+    id: () => {
+      new Date();
+    },
+    label: "",
+    recordType: "Локальная",
+    login: "",
+    password: "",
+  });
+};
+
+const updateAccount = (id: number, updatedAccount: Account) => {
+  accounts.value[id] = updatedAccount;
+};
+
+const deleteAccount = (id: number) => {
+  accounts.value.splice(id, 1);
+};
 </script>
