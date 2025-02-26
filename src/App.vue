d7ca89967f44f4535ec4cb8cc8344216ebaa98e5
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-26 20:15:31 +0300

    Git inited

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <h1>Hello!</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo optio
+      eveniet aspernatur vero consectetur quod ex praesentium itaque eligendi
+      distinctio deserunt libero, quis magnam beatae perferendis provident
+      soluta nam a?
+    </p>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
