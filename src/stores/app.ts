3552e9b86fb8bd741c05955447894fa331287c47
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-28 01:12:14 +0300

    Updated store 'app.ts' -  Deleted new actions 'loadAccountsFromLocalStorage' and 'saveAccountsToLocalStorage'

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,8 +1,6 @@
 import { defineStore } from "pinia";
 import type { Account } from "@/types/account";
 
-const STORAGE_KEY = "account_management_accounts";
-
 export const useAppStore = defineStore("app", {
   state: () => ({
     accounts: [
@@ -48,48 +46,29 @@ export const useAppStore = defineStore("app", {
     getAccounts: (state) => state.accounts,
   },
   actions: {
-    // Функция загрузки данных из localStorage при инициализации стора
-    loadAccountsFromLocalStorage() {
-      const storedAccounts = localStorage.getItem(STORAGE_KEY);
-      if (storedAccounts) {
-        try {
-          this.accounts = JSON.parse(storedAccounts);
-        } catch (error) {
-          // Обработка ошибок при парсинге JSON
-          console.error("Error parsing accounts from localStorage:", error);
-          localStorage.removeItem(STORAGE_KEY);
-        }
-      }
-    },
-    // Функция сохранения данных в localStorage после каждого изменения
-    saveAccountsToLocalStorage() {
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.accounts));
-    },
     addAccount(account: Account) {
       // Создаем глубокую копию объекта
       this.accounts.push(JSON.parse(JSON.stringify(account)));
-      this.saveAccountsToLocalStorage();
     },
     updateAccount(account: Account) {
       console.log("Store updating account:", account);
       const index = this.accounts.findIndex((a) => a.id === account.id);
       if (index !== -1) {
         this.accounts[index] = JSON.parse(JSON.stringify(account));
-        this.saveAccountsToLocalStorage();
       }
     },
     deleteAccount(id: string) {
       this.accounts = this.accounts.filter((account) => account.id !== id);
-      this.saveAccountsToLocalStorage();
     },
   },
-  // persist: {
-  //   enabled: true,
-  //   strategies: [
-  //     {
-  //       key: "account_management_accounts",
-  //       storage: localStorage,
-  //     },
-  //   ],
-  // },
+
+  persist: {
+    enabled: true,
+    strategies: [
+      {
+        key: "account_management_accounts",
+        storage: localStorage,
+      },
+    ],
+  },
 });
