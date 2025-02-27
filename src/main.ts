444809f6e845c46d0a67b9530a4b53f192ff03b1
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-27 13:47:11 +0300

    Updated 'main.ts' - Added import and use 'pinia'

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { registerPlugins } from "@/plugins";
 import App from "./App.vue";
 import { createApp } from "vue";
+import pinia from "./stores";
 
 const app = createApp(App);
 
+app.use(pinia);
 registerPlugins(app);
 
 app.mount("#app");
