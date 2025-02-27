840f61d345d9568c182041f47c32fdb07060514e
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-27 17:22:03 +0300

    Updated component 'AccountItem' - Updated 'labelRules' Added new function: 'validateField', 'onLabelBlur' and 'onPasswordBlur'. Updated 'watch'

--- src/components/AccountItem.vue
+++ src/components/AccountItem.vue
@@ -10,6 +10,7 @@
                 v-model="localLabel"
                 :rules="labelRules"
                 @blur="onLabelBlur"
+                :error="labelError"
                 hint="Метки вводятся через знак ';'"
               ></v-text-field>
             </v-col>
@@ -28,7 +29,8 @@
                 label="Логин"
                 v-model="localAccount.login"
                 :rules="loginRules"
-                @blur="onUpdate"
+                @blur="onLoginBlur"
+                :error="loginError"
               ></v-text-field>
             </v-col>
 
@@ -38,7 +40,8 @@
                 label="Пароль"
                 v-model="localAccount.password"
                 :rules="passwordRules"
-                @blur="onUpdate"
+                @blur="onPasswordBlur"
+                :error="passwordError"
               ></v-text-field>
             </v-col>
           </v-row>
@@ -55,14 +58,14 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onMounted } from "vue";
+import { ref, watch, onMounted, computed } from "vue";
 
 interface Account {
   id: string;
   label: string;
   recordType: string;
   login: string;
-  password?: string;
+  password?: string | null;
 }
 
 const props = defineProps<{
@@ -76,24 +79,67 @@ const emit = defineEmits<{
 
 const localAccount = ref<Account>({ ...props.account });
 const localLabel = ref(props.account.label); // Отдельная ссылка для метки
-
 const recordTypes = ref(["LDAP", "Локальная"]);
 
+// ref для отслеживания ошибок валидации
+const loginError = ref(false);
+const passwordError = ref(false);
+const labelError = ref(false);
+
+// Правила валидации:
 const labelRules = ref([
-  (v: any) =>
-    !v ||
-    (v.length <= 50 && "Метка вводятся через знак ';'") ||
-    "Метка должна быть не больше 50 символов",
+  (v: any) => {
+    if (v) {
+      return v.length <= 50 || "Метка должна быть не больше 50 символов";
+    }
+    return true; // Не проверяем, если пусто.
+  },
 ]);
 const loginRules = ref([
   (v: any) => !!v || "Логин обязателен для заполнения",
-  (v: any) => v.length <= 100 || "Логин должен быть не больше 100 символов",
+  (v: any) => {
+    if (v) {
+      return v.length <= 100 || "Логин должен быть не больше 100 символов";
+    }
+    return true;
+  },
 ]);
 const passwordRules = ref([
   (v: any) => !!v || "Пароль обязателен для заполнения",
-  (v: any) => v.length <= 100 || "Пароль должен быть не больше 100 символов",
+  (v: any) => {
+    if (v) {
+      return v.length <= 100 || "Пароль должен быть не больше 100 символов";
+    }
+    return true;
+  },
 ]);
 
+// Функция валидации:
+const validateField = (fieldName: "login" | "password" | "label") => {
+  switch (fieldName) {
+    case "login":
+      loginError.value = !loginRules.value.every((rule) => {
+        const result =
+          typeof rule === "function" ? rule(localAccount.value.login) : true;
+        return result === true || result === undefined;
+      });
+      break;
+    case "password":
+      passwordError.value = !passwordRules.value.every((rule) => {
+        const result =
+          typeof rule === "function" ? rule(localAccount.value.password) : true;
+        return result === true || result === undefined;
+      });
+      break;
+    case "label":
+      labelError.value = !labelRules.value.every((rule) => {
+        const result =
+          typeof rule === "function" ? rule(localLabel.value) : true;
+        return result === true || result === undefined;
+      });
+      break;
+  }
+};
 const onRecordTypeChange = () => {
   if (localAccount.value.recordType === "LDAP") {
     localAccount.value.password = null; // Устанавливаем пароль на null
@@ -101,19 +147,29 @@ const onRecordTypeChange = () => {
   onUpdate();
 };
 
-const onDelete = () => {
-  emit("delete");
+const onLabelBlur = () => {
+  validateField("label");
+  onUpdate();
 };
 
-const onLabelBlur = () => {
+const onLoginBlur = () => {
+  validateField("login");
+  onUpdate();
+};
+
+const onPasswordBlur = () => {
+  validateField("password");
   onUpdate();
 };
 
 const onUpdate = () => {
-  // Преобразование строки метки в массив объектов
+  // Преобразование метки в массив объектов
   localAccount.value.label = localLabel.value;
-
   emit("update", localAccount.value);
+};
+
+const onDelete = () => {
+  emit("delete");
 };
 
 watch(
@@ -121,6 +177,9 @@ watch(
   (newAccount) => {
     localAccount.value = { ...newAccount };
     localLabel.value = newAccount.label;
+    if (newAccount.label) {
+      localLabel.value = newAccount.label;
+    }
   },
   { immediate: true, deep: true }
 );
