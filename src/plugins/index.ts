8f2fee13cc77d1679906aecdced4f0728e5266e6
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-27 13:45:49 +0300

    Updated plugins: 'index.ts'

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,10 +1,13 @@
 // Plugins
 import vuetify from "./vuetify";
 import pinia from "../stores";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persist";
 
 // Types
 import type { App } from "vue";
 
 export function registerPlugins(app: App) {
+  pinia.use(piniaPluginPersistedstate);
   app.use(vuetify).use(pinia);
 }
