d78fc7418cfb1c695d3a3fdecf755ba277077bfc
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-27 19:03:58 +0300

    Updated interface 'Account' from '/types/account.ts'

--- src/types/account.ts
+++ src/types/account.ts
@@ -1,6 +1,6 @@
 export interface Account {
   id: string;
-  label: string;
+  label: { text: string }[]; // label является массивом объектов, где каждый объект имеет поле text типа string
   recordType: string;
   login: string;
   password?: string | null | undefined;
