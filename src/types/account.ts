65c5934dd0d549c0af83420309d9a6713b1ec656
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-28 10:33:27 +0300

    Updated 'types/account.ts' - Added 'labelString' in interface 'Account'

--- src/types/account.ts
+++ src/types/account.ts
@@ -1,6 +1,7 @@
 export interface Account {
   id: string;
   label: { text: string }[]; // label является строкой или массивом объектов, где каждый объект имеет поле text типа string
+  labelString?: string;
   recordType: string;
   login: string;
   password?: string | null;
